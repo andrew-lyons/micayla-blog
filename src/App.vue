ea9cc7af7923ed3cc1d8c24d33050f9c637f0968
Andrew Lyons <aslyons001@gmail.com>
2022-09-20 22:38:55 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
+
+  <header>
+    <h1>
+      Micayla
+    </h1>
+  </header>
+
   <router-view/>
 </template>
 
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+</style>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  font-family: 'Montserrat', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: rgb(249, 247, 245);
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  h1 {
+    font-size: 60px;
+    font-weight: 100;
+    letter-spacing: 5px;
+    line-height: 2.5;
+    margin-bottom: 10px;
+    font-family: 'Montserrat', sans-serif;
+  }
 }
 
 nav {
-  padding: 30px;
+  display: flex;
+  justify-content: center;
+  color: #5d5d5d;
+  font-weight: 600;
+  text-align: center;
+  background: #ffffff;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  font-size: 12px;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    color: #5d5d5d;
+    font-weight: 600;
+    text-align: center;
+    background: #ffffff;
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    font-size: 12px;
+    padding: 20px;
+    text-decoration: none;
   }
 }
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,22 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="homepage">
+    <div class="homepage-hero">
+      <div class="homepage-hero-image">
+        <img src="https://jane.brandingmypurpose.com/wp-content/uploads/2022/06/profile.png" alt="">
+      </div>
+      <div class="homepage-hero-text">
+        <div class="homepage-hero-text-divider"><span></span></div>
+  
+        <h1>hello and welcome!</h1>
+  
+        <p>
+          Hi there, I'm Jane. This is a demo installation for the premade branding 
+          and WordPress design created by Kelly Brito. It's perfect for minimal and 
+          feminine brands.
+        </p>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -45,18 +33,54 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1 {
+    color: #363636;
+    font-family: "Montserrat", Sans-serif;
+    font-size: 52px;
+    font-weight: 200;
+  }
+
+  p {
+    text-align: center;
+    color: #363636;
+    font-family: "Montserrat", Sans-serif;
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 2.1em;
+    letter-spacing: 2px;
+  }
+
+  .homepage {
+    &-hero {
+      display: flex;
+      align-items: center;
+      padding: 0 80px;
+
+      &-image {
+        width: 53.47%;
+        transform: scaleX(-1);
+
+        img {
+          width: 100%;
+          height: auto;
+        }
+      }
+      &-text {
+        width: 46.53%;
+
+        &-divider {
+          display: flex;
+          text-align: center;
+          padding-top: 15px;
+          padding-bottom: 15px;
+  
+          span {
+            width: 10%;
+            margin: 0 auto;
+            border-top: 1.8px solid #E2CECC;
+          }
+        }
+      }
+    }
+  }
 </style>
