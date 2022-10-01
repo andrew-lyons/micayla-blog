38f71838a9ccbf5a0df67bdb75fde4248dd21910
Andrew Lyons <aslyons001@gmail.com>
2022-10-01 19:39:14 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ echo 'www.themarigoldproject.com' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploying...'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
